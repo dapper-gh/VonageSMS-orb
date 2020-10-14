212ace9c338cf9f9964c4fbaa6a31589828a4cd7
David Stroud <me@dapper.codes>
2020-10-14 17:33:48 -0400

    Fix error code parsing
    Use "jq" and other tricks to parse errors correctly (thank you Vonage for not properly responding with status codes!)

--- src/scripts/smscurl.sh
+++ src/scripts/smscurl.sh
@@ -1,7 +1,10 @@
-curl -X "POST" "https://rest.nexmo.com/sms/json" \
--d "from=$VONAGE_NUMEBR" \
--d "text=$VONAGE_MESSAGE" \
--d "to=$VONAGE_RECIPIENT" \
--d "api_key=$VONAGE_API_KEY" \
--d "api_secret=$VONAGE_API_SECRET"
-exit $?
+CURL_RESPONSE=$(curl -X "POST" "https://rest.nexmo.com/sms/json" \
+  -d "from=$VONAGE_NUMEBR" \
+  -d "text=$VONAGE_MESSAGE" \
+  -d "to=$VONAGE_RECIPIENT" \
+  -d "api_key=$VONAGE_API_KEY" \
+  -d "api_secret=$VONAGE_API_SECRET")
+EXIT_CODE=$(echo "$CURL_RESPONSE" | jq -r ".messages[0].status")
+echo "-- CURL RESPONSE BELOW --"
+echo "$CURL_RESPONSE"
+exit "$EXIT_CODE"
